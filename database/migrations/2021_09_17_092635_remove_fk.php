8a29ed5351dffc6004597f19f1c80944fb13c5dc
paul chou <paul_chou@mail.chungyo.net>
2021-10-14 16:33:32 +0800

    [feature]移除全部外鍵

--- database/migrations/2021_09_17_092635_remove_fk.php
+++ database/migrations/2021_09_17_092635_remove_fk.php
@@ -14,6 +14,12 @@ class RemoveFk extends Migration
     public function up()
     {
 
+        Schema::table('sport', function (Blueprint $table) {
+            $table->dropForeign(['sport_category_id']);
+            $table->dropForeign(['sport_type_id']);
+            $table->dropForeign(['sport_play_id']);
+        });
+
         Schema::table('game', function (Blueprint $table) {
             $table->dropForeign(['main_team_id']);
             $table->dropForeign(['visit_team_id']);
@@ -28,6 +34,14 @@ class RemoveFk extends Migration
             $table->dropForeign(['sport_id']);
             $table->dropForeign(['game_id']);
         });
+
+        Schema::table('sport_league', function (Blueprint $table) {
+            $table->dropForeign(['sport_category_id']);
+        });
+
+        Schema::table('sport_team', function (Blueprint $table) {
+            $table->dropForeign(['sport_league_id']);
+        });
     }
 
     /**
@@ -37,6 +51,12 @@ class RemoveFk extends Migration
      */
     public function down()
     {
+        Schema::table('sport', function (Blueprint $table) {
+            $table->foreign('sport_category_id')->references('id')->on('sport_category');
+            $table->foreign('sport_type_id')->references('id')->on('sport_type');
+            $table->foreign('sport_play_id')->references('id')->on('sport_play');
+        });
+
         Schema::table('game_bet_status', function (Blueprint $table) {
             $table->foreign('sport_id')->references('id')->on('sport');
             $table->foreign('game_id')->references('id')->on('game');
@@ -50,6 +70,14 @@ class RemoveFk extends Migration
         Schema::table('game', function (Blueprint $table) {
             $table->foreign('main_team_id')->references('id')->on('sport_team');
             $table->foreign('visit_team_id')->references('id')->on('sport_team');
+        });
+
+        Schema::table('sport_league', function (Blueprint $table) {
+            $table->foreign('sport_category_id')->references('id')->on('sport_category');
+        });
+
+        Schema::table('sport_team', function (Blueprint $table) {
+            $table->foreign('sport_league_id')->references('id')->on('sport_league');
         });
     }
 }
